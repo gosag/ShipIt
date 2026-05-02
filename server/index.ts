375719153286a93f012fb48f5a3853a162ea39f2
Gosa Girma <gosagirma441@gmail.com>
2026-05-02 19:19:43 +0300

    feat: configure CORS options for the server

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,13 @@ import morgan from "morgan"
 dotenv.config()
 const app=express()
 const PORT= process.env.PORT || 8000
-app.use(cors())
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  };
+app.use(cors(corsOptions))
 app.use(morgan("dev"))
 app.listen(PORT,()=>{
     console.log("Server is started")
