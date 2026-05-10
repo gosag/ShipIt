e644e3ae9b5f63edbeaf09d3cffd0881733cd626
Gosa Girma <gosagirma441@gmail.com>
2026-05-10 08:22:18 +0300

    feat: add react-router-dom dependency and implement registration form with validation

---
+++ client/src/auth/register.tsx
@@ -0,0 +1,42 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+const Register=()=>{
+    const navigate = useNavigate();
+    type FormData = z.infer<typeof schema>;
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+    const onSubmit = async (data: FormData) => {
+        try{
+            const result= await fetch(`${import.meta.env.VITE_URL}/register`,{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(data)
+            })
+            const res= await result.json()
+            if(!res.ok){
+                throw new Error("Something went wrong ")
+            }
+        }catch(err){
+            console.log(err)
+        }
+    }
+    return(
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" placeholder="Email" {...register("email")} />
+                {errors.email && <p>{errors.email.message}</p>}
+                <input type="password" placeholder="Password" {...register("password")} />
+                {errors.password && <p>{errors.password.message}</p>}
+                <input type="submit">Submit</input>
+            </form>
+        </div>
+    )
+};
+export default Register;
