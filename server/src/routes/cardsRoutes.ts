19e7851c4804ba42133510a907457842bcc0ba4e
Gosa Girma <gosagirma441@gmail.com>
2026-05-18 17:53:59 +0300

    feat(cards): add moveCard and deleteCard routes to cardsRouter

--- server/src/routes/cardsRoutes.ts
+++ server/src/routes/cardsRoutes.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { authenticate } from "../middleware/auth.js";
-import { createCard, updateCard} from "../controllers/cardsController.js";
+import { createCard, updateCard, moveCard, deleteCard} from "../controllers/cardsController.js";
 const cardsRouter= express.Router();
 cardsRouter.post("/:workspaceId/projects/:projectId/columns/:columnId/cards", authenticate, createCard);
-cardsRouter.put("columns/:columnId/cards:cardId",authenticate, updateCard)
+cardsRouter.put("columns/:columnId/cards:cardId",authenticate, updateCard);
+cardsRouter.put("columns/:columnId/cards:cardId/move",authenticate, moveCard);
+cardsRouter.delete("columns/:columnId/cards:cardId",authenticate, deleteCard);
+
 export default cardsRouter;
