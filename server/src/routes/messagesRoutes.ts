3b7661d37a1094a7a402ada5b00efe64e5c78842
Gosa Girma <gosagirma441@gmail.com>
2026-06-02 13:39:33 +0300

    feat(messages): update message routes to include cardId parameter

--- server/src/routes/messagesRoutes.ts
+++ server/src/routes/messagesRoutes.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { authenticate } from '../middleware/auth.js';
 import { getComments, sendComment } from '../controllers/messagesController.js';
 const messagesRouter = express.Router();
-messagesRouter.get('/get-messages', authenticate, getComments);
-messagesRouter.post('/send-message', authenticate, sendComment);
+messagesRouter.get('/get-messages/:cardId', authenticate, getComments);
+messagesRouter.post('/send-message/:cardId', authenticate, sendComment);
 export default messagesRouter;
